0ce0bcc883baec4b3d0a87c05cce431ead8e5a86
Miguel-b <109560374+Miguel-b@users.noreply.github.com>
2025-11-20 14:58:43 -0500

    errores

--- Capitulo2/utils.py
+++ Capitulo2/utils.py
@@ -4,8 +4,10 @@ def jacobi(A, b, x0, tol, niter):
     n = len(A)
     x = x0.copy()
     iteraciones = []
+    # Número de condición de la matriz (constante durante el método)
+    condicion = np.linalg.cond(A) 
 
-    for i in range(len(A)):
+    for i in range(n):
         if A[i][i] == 0:
             raise ValueError(f"Jacobi no puede ejecutarse: A[{i}][{i}] = 0 provoca división por cero.")
 
@@ -14,24 +16,28 @@ def jacobi(A, b, x0, tol, niter):
         for i in range(n):
             suma = sum(A[i][j] * x[j] for j in range(n) if j != i)
             x_new[i] = (b[i] - suma) / A[i][i]
-        error = np.linalg.norm(np.array(x_new) - np.array(x))
 
-        iteraciones.append([k+1, *x_new, error])
-        if error < tol:
+        # Errores
+        e_abs_vec = np.abs(np.array(x_new) - np.array(x))
+        error_abs = np.max(e_abs_vec)
+        relative_err1 = np.max(np.abs(e_abs_vec / np.where(np.array(x_new)!=0, np.array(x_new), 1e-12)))
+        relative_err2 = np.max(np.abs(e_abs_vec / np.where(np.array(x)!=0, np.array(x), 1e-12)))
+
+        iteraciones.append([k+1, *x_new, error_abs, relative_err1, relative_err2, condicion])
+        if error_abs < tol:
             break
         x = x_new
     return iteraciones
-
 
 def gauss_seidel(A, b, x0, tol, niter):
     n = len(A)
     x = x0.copy()
     iteraciones = []
-    
-    for i in range(len(A)):
+    condicion = np.linalg.cond(A)
+
+    for i in range(n):
         if A[i][i] == 0:
             raise ValueError(f"Gauss-seidel no puede ejecutarse: A[{i}][{i}] = 0 provoca división por cero.")
-
 
     for k in range(niter):
         x_new = x.copy()
@@ -39,23 +45,28 @@ def gauss_seidel(A, b, x0, tol, niter):
             suma1 = sum(A[i][j] * x_new[j] for j in range(i))
             suma2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
             x_new[i] = (b[i] - suma1 - suma2) / A[i][i]
-        error = np.linalg.norm(np.array(x_new) - np.array(x))
-        iteraciones.append([k+1, *x_new, error])
-        if error < tol:
+
+        # Errores
+        e_abs_vec = np.abs(np.array(x_new) - np.array(x))
+        error_abs = np.max(e_abs_vec)
+        relative_err1 = np.max(np.abs(e_abs_vec / np.where(np.array(x_new)!=0, np.array(x_new), 1e-12)))
+        relative_err2 = np.max(np.abs(e_abs_vec / np.where(np.array(x)!=0, np.array(x), 1e-12)))
+
+        iteraciones.append([k+1, *x_new, error_abs, relative_err1, relative_err2, condicion])
+        if error_abs < tol:
             break
         x = x_new
     return iteraciones
-
 
 def sor(A, b, x0, tol, niter, w):
     n = len(A)
     x = x0.copy()
     iteraciones = []
-    
-    for i in range(len(A)):
+    condicion = np.linalg.cond(A)
+
+    for i in range(n):
         if A[i][i] == 0:
             raise ValueError(f"SOR no puede ejecutarse: A[{i}][{i}] = 0 provoca división por cero.")
-
 
     for k in range(niter):
         x_new = x.copy()
@@ -63,10 +74,15 @@ def sor(A, b, x0, tol, niter, w):
             suma1 = sum(A[i][j] * x_new[j] for j in range(i))
             suma2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
             x_new[i] = (1 - w) * x[i] + (w / A[i][i]) * (b[i] - suma1 - suma2)
-        error = np.linalg.norm(np.array(x_new) - np.array(x))
 
-        iteraciones.append([k+1, *x_new, error])
-        if error < tol:
+        # Errores
+        e_abs_vec = np.abs(np.array(x_new) - np.array(x))
+        error_abs = np.max(e_abs_vec)
+        relative_err1 = np.max(np.abs(e_abs_vec / np.where(np.array(x_new)!=0, np.array(x_new), 1e-12)))
+        relative_err2 = np.max(np.abs(e_abs_vec / np.where(np.array(x)!=0, np.array(x), 1e-12)))
+
+        iteraciones.append([k+1, *x_new, error_abs, relative_err1, relative_err2, condicion])
+        if error_abs < tol:
             break
         x = x_new
     return iteraciones
